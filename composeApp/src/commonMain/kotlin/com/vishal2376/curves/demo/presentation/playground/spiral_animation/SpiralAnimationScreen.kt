20c9921eb0309cf2c4f147a3d149b9e61ca5eb21
Vishal Singh <vishalsingh2376@gmail.com>
2025-10-30 21:45:58 +0530

    feat: improve spiral animation

--- composeApp/src/commonMain/kotlin/com/vishal2376/curves/demo/presentation/playground/spiral_animation/SpiralAnimationScreen.kt
+++ composeApp/src/commonMain/kotlin/com/vishal2376/curves/demo/presentation/playground/spiral_animation/SpiralAnimationScreen.kt
@@ -1,105 +1,93 @@
 package com.vishal2376.curves.demo.presentation.playground.spiral_animation
 
-import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.EaseInOut
+import androidx.compose.animation.core.InfiniteRepeatableSpec
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
-import com.vishal2376.curves.demo.presentation.common.components.CustomLabel
+import androidx.compose.ui.util.lerp
+import com.vishal2376.curves.core.presentation.theme.CurvesTheme
 import com.vishal2376.curves.demo.presentation.common.utils.polarLineTo
+import com.vishal2376.curves.demo.presentation.common.utils.polarMoveTo
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SpiralAnimationScreen() {
 
 	val colors = MaterialTheme.colorScheme
 
-	val res = 100
-	var degree by remember {
-		mutableStateOf(0f)
-	}
-	var distance by remember {
-		mutableStateOf(0f)
-	}
-
-	val resAnim by rememberInfiniteTransition().animateFloat(
-		initialValue = 10f,
-		targetValue = res.toFloat(),
-		animationSpec = infiniteRepeatable(
-			animation = tween(
-				durationMillis = 3000,
-				easing = LinearEasing
-			),
-			repeatMode = RepeatMode.Reverse
-		),
-		label = ""
+	val infiniteAnimation = rememberInfiniteTransition("spiral")
+	val progress by infiniteAnimation.animateFloat(
+		initialValue = 0f,
+		targetValue = 1f,
+		animationSpec = InfiniteRepeatableSpec(
+			repeatMode = RepeatMode.Reverse,
+			animation = tween(durationMillis = 3000, easing = EaseInOut)
+		)
 	)
-
 
 	Column(
 		modifier = Modifier.fillMaxSize()
 			.background(colors.background)
 			.padding(16.dp),
 		horizontalAlignment = Alignment.CenterHorizontally,
-		verticalArrangement = Arrangement.SpaceBetween
+		verticalArrangement = Arrangement.Center
 	) {
+		val resolution = 500
+		val loops = 5
 
-		Box(
-			modifier = Modifier.fillMaxWidth().weight(1f),
-			contentAlignment = Alignment.Center
+		Canvas(
+			modifier = Modifier.aspectRatio(1f)
 		) {
+			val path = Path()
+			(0..resolution).forEach { i ->
+				val t = (i / resolution.toFloat()) * progress
+				val degree = (loops * 360f) * t
 
-			Canvas(
-				modifier = Modifier
-			) {
-				val distanceConst = 100f
+				val distance = lerp(
+					start = 0f,
+					stop = size.minDimension / 2,
+					fraction = t
+				)
 
-				val path = Path()
-				(0..res).forEach { i ->
-					val t = i / resAnim
-					degree = t * 360f
-					distance = distanceConst * t
-					path.apply {
-						polarLineTo(degree, distance)
-					}
+				if (i == 0) {
+					path.polarMoveTo(0f, 0f, center)
+				} else {
+					path.polarLineTo(degree, distance, origin = center)
 				}
 
-				drawPath(
-					path = path,
-					color = colors.primary,
-					style = Stroke(width = 10f)
-				)
 			}
+
+			drawPath(
+				path = path,
+				color = colors.primary,
+				style = Stroke(width = 10f)
+			)
 		}
-		Column(
-			modifier = Modifier.fillMaxWidth()
-				.background(colors.surface, RoundedCornerShape(24.dp))
-				.padding(24.dp),
-			horizontalAlignment = Alignment.CenterHorizontally
-		) {
-			CustomLabel("Resolution", resAnim.toInt())
-			CustomLabel("Degree", degree.toInt())
-			CustomLabel("Distance", distance.toInt())
-		}
+	}
+}
+
+
+@Preview
+@Composable
+fun SpiralAnimationScreenPreview() {
+	CurvesTheme(darkTheme = true, dynamicColor = false) {
+		SpiralAnimationScreen()
 	}
 }
