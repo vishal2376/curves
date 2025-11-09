f56f90afadb604d6de429bec9a9e932267e23a25
Vishal Singh <vishalsingh2376@gmail.com>
2025-11-10 01:39:45 +0530

    feat: set window size and align center

--- composeApp/src/jvmMain/kotlin/com/vishal2376/curves/main.kt
+++ composeApp/src/jvmMain/kotlin/com/vishal2376/curves/main.kt
@@ -1,14 +1,23 @@
 package com.vishal2376.curves
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() {
 	application {
 		Window(
 			onCloseRequest = ::exitApplication,
 			title = "KMP Curves",
+			state = rememberWindowState(
+				size = DpSize(1280.dp, 920.dp),
+				position = WindowPosition(Alignment.Center)
+			)
 		) {
 			App(darkTheme = isSystemInDarkTheme(), dynamicColor = false)
 		}
